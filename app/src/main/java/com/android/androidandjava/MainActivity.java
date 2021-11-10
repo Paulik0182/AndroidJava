dd072c7fac0491ee387633f942d6370539a80ffb
Paveł <Paulik0182@gmail.com>
2021-11-10 16:32:45 +0300

    Layout - пример appCompatImageView, клик по иконке

--- app/src/main/java/com/android/androidandjava/MainActivity.java
+++ app/src/main/java/com/android/androidandjava/MainActivity.java
@@ -1,14 +1,27 @@
 package com.android.androidandjava;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppCompatImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
-        setContentView ( R.layout.activity_main );
+        setContentView ( R.layout.activity_frame_appcompatimageview_loyout );
+
+        imageView = findViewById ( R.id.image_view1 );
+
+        imageView.setOnClickListener ( new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                imageView.setImageResource ( R.drawable.ic_launcher_background );
+            }
+        } );
     }
 }
