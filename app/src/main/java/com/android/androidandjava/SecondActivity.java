5b66d85b5cb6858b4b1fea0384167b1b0c4f43ee
Paveł <Paulik0182@gmail.com>
2021-11-11 12:28:26 +0300

    Жизненый цыкл Activity_2 - тестируем вызов функций при двух Activity (onCreate, onStart, onResume, onPause, onStop, onDestroy). Проверка их запуска через Log. Доп создано: SecondActivity, Layout, Button.

--- app/src/main/java/com/android/androidandjava/SecondActivity.java
+++ app/src/main/java/com/android/androidandjava/SecondActivity.java
@@ -1,25 +1,17 @@
 package com.android.androidandjava;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "@@@ MainActivity"; //@@@ пометка по каторой будем искать, далее файл в который будем писать
+public class SecondActivity extends AppCompatActivity {
+    private static final String TAG = "@@@ SecondActivity"; //@@@ пометка по каторой будем искать, далее файл в который будем писать
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
-        setContentView ( R.layout.activity_main );
-        findViewById ( R.id.button_activity2 ).setOnClickListener ( v -> {
-            Intent intent = new Intent ( this, SecondActivity.class );
-            startActivity ( intent );
-
-            Log.d ( TAG, "Send Intent MainActivity -> SecondActivity" );
-        } );
-
+        setContentView ( R.layout.activity_second );
         Log.d ( TAG, "onCreate" );
     }
 
