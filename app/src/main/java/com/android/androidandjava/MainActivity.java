366b43e97154f1944d4d976b529178bfaf006af0
Paveł <Paulik0182@gmail.com>
2021-11-11 11:30:47 +0300

    Жизненый цыкл Activity - onCreate, onStart, onResume, onPause, onStop, onDestroy. Проверка их запуска через Log.

--- app/src/main/java/com/android/androidandjava/MainActivity.java
+++ app/src/main/java/com/android/androidandjava/MainActivity.java
@@ -1,14 +1,48 @@
 package com.android.androidandjava;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "@@@ MainActivity"; //@@@ пометка по каторой будем искать, далее файл в который будем писать
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_main );
+
+        Log.d ( TAG, "onCreate" );
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart ();
+        Log.d ( TAG, "onStart" );
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart ();
+        Log.d ( TAG, "onRestart" );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause ();
+        Log.d ( TAG, "onPause" );
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop ();
+        Log.d ( TAG, "onStop" );
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy ();
+        Log.d ( TAG, "onDestroy" );
     }
 }
