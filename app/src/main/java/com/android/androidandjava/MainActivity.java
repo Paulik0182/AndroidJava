4af9fbb1317fe762784ef5f314797bdc4a107bd8
Paveł <Paulik0182@gmail.com>
2021-11-16 23:44:00 +0300

    применение ресурсов и стилей

--- app/src/main/java/com/android/androidandjava/MainActivity.java
+++ app/src/main/java/com/android/androidandjava/MainActivity.java
@@ -1,14 +1,20 @@
 package com.android.androidandjava;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
-        setContentView ( R.layout.activity_main );
+//        setContentView ( R.layout.activity_main );
+        setContentView ( R.layout.activity_test_thema );
+
+//        TextView textView = findViewById ( R.id.text_view );
+//        textView.setText ( R.string.app_name );
+//        String string = this.getResources ().getString ( R.string.greetings );
+//        textView.setText ( string );
     }
 }
