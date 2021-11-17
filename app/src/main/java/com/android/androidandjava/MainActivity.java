7e90b75585e32a1d24023c387d0f9770b3b79670
Paveł <Paulik0182@gmail.com>
2021-11-17 17:41:11 +0300

    интенты и активити - создал еще одну Activity и Layout

--- app/src/main/java/com/android/androidandjava/MainActivity.java
+++ app/src/main/java/com/android/androidandjava/MainActivity.java
@@ -1,14 +1,31 @@
 package com.android.androidandjava;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private TextView echoTextView;
+    private EditText messageEditText;
+    private Button nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_main );
+
+        echoTextView.findViewById ( R.id.echo_text_view );
+        messageEditText.findViewById ( R.id.message_edit_text );
+        nextButton.findViewById ( R.id.next_button );
+
+        nextButton.setOnClickListener ( v -> {
+            Intent intent = new Intent ( this, SecondActivity.class );
+            startActivity ( intent );
+        } );
+
     }
 }
