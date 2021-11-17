476fee56821a30e0a0fd7c00082cadceab8782e3
Paveł <Paulik0182@gmail.com>
2021-11-17 11:01:24 +0300

    Material design - применение старонних библиотек для текстового поля.

--- app/src/main/java/com/android/androidandjava/MainActivity.java
+++ app/src/main/java/com/android/androidandjava/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,6 +14,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate ( savedInstanceState );
 //        setContentView ( R.layout.activity_main );
         setContentView ( R.layout.activity_test_thema );
+
+
+        TextInputLayout textInputLayout = findViewById ( R.id.text_input_layout );
+        TextInputEditText textInputEditText = findViewById ( R.id.text_input_edit_text );
+
+        textInputEditText.setOnFocusChangeListener ( (v, hasFocus) -> textInputLayout.setError ( "Ошибка!" ) );
+
+//        textInputEditText.setOnFocusChangeListener ( new View.OnFocusChangeListener () {//до приобразовании в лямду. см.выше
+//            @Override
+//            public void onFocusChange(View v, boolean hasFocus) {
+//                textInputLayout.setError ("Ошибка!");
+//            }
+//        } );
 
 //        TextView textView = findViewById ( R.id.text_view );
 //        textView.setText ( R.string.app_name );
