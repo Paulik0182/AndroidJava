f95be8082619e1670e7477112cb6b22c62ee38fa
Paveł <Paulik0182@gmail.com>
2021-11-22 23:47:22 +0300

    Fragment - создан фрагмент и по нажатию на кнопку, он появляется в activity_main

--- app/src/main/java/com/android/androidandjava/MainActivity.java
+++ app/src/main/java/com/android/androidandjava/MainActivity.java
@@ -1,14 +1,31 @@
 package com.android.androidandjava;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private Button showButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_main );
+
+        showButton = findViewById ( R.id.show_fragment_button );
+
+        showButton.setOnClickListener ( new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                ProfileFragment profileFragment = new ProfileFragment ();
+                getSupportFragmentManager ()
+                        .beginTransaction ()
+                        .add ( R.id.fragment_container, profileFragment )
+                        .commit ();
+            }
+        } );
     }
 }
