5abb1914c74e1d04821fdc926561a1e711b51a2b
Paveł <Paulik0182@gmail.com>
2021-11-23 14:13:55 +0300

    Fragment - залогировали этапы создания  и т.д. активити и фрагмента, передачи данных. При повороте, приложение падает, не может найти конструктор.

--- app/src/main/java/com/android/androidandjava/MainActivity.java
+++ app/src/main/java/com/android/androidandjava/MainActivity.java
@@ -1,6 +1,7 @@
 package com.android.androidandjava;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,18 +15,50 @@ public class MainActivity extends AppCompatActivity {
 
     private Button showButton;
 
+    private static final String TAG = "@@@ MainActivity";// константа для лога
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_main );
 
         showButton = findViewById ( R.id.show_fragment_button );
-
+        Log.d ( TAG, "onCreate() called with: savedInstanceState = [" + savedInstanceState + "]" );
         showButton.setOnClickListener ( v -> {
             getSupportFragmentManager ()
                     .beginTransaction ()
                     .add ( R.id.fragment_container, profileFragment )
                     .commit ();
         } );
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart ();
+        Log.d ( TAG, "onStart() called" );
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume ();
+        Log.d ( TAG, "onResume() called" );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause ();
+        Log.d ( TAG, "onPause() called" );
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop ();
+        Log.d ( TAG, "onStop() called" );
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy ();
+        Log.d ( TAG, "onDestroy() called" );
     }
 }
--- app/src/main/java/com/android/androidandjava/ProfileFragment.java
+++ app/src/main/java/com/android/androidandjava/ProfileFragment.java
@@ -1,6 +1,8 @@
 package com.android.androidandjava;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,23 +18,35 @@ public class ProfileFragment extends Fragment {
     private EditText surnameEt;
     private EditText emailEt;
 
+    private static final String TAG = "@@@ ProfileFragment";// константа для лога
+
+
     public ProfileFragment(DossierEntity dossierEntity) {//создали конструктор в классе и присвоили данные конструктора DossierEntity
         dossier = dossierEntity;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        Log.d ( TAG, "onCreate() called with: savedInstanceState = [" + savedInstanceState + "]" );
         super.onCreate ( savedInstanceState );
     }
 
     @Override
+    public void onDestroy() {
+        Log.d ( TAG, "onDestroy() called" );
+        super.onDestroy ();
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.d ( TAG, "onCreateView() called with: inflater = [" + inflater + "], container = [" + container + "], savedInstanceState = [" + savedInstanceState + "]" );
         View view = inflater.inflate ( R.layout.fragment_profile, null );// обратились к фрагменту (создали фрагмент)
         return view;
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
+        Log.d ( TAG, "onViewCreated() called with: view = [" + view + "], savedInstanceState = [" + savedInstanceState + "]" );
 
         // инициализировали EditText
         nameEt = view.findViewById ( R.id.name_edit_text );
@@ -43,5 +57,47 @@ public class ProfileFragment extends Fragment {
         nameEt.setText ( dossier.name );
         surnameEt.setText ( dossier.surname );
         emailEt.setText ( dossier.email );
+    }
+
+    @Override
+    public void onDestroyView() {
+        Log.d ( TAG, "onDestroyView() called" );
+        super.onDestroyView ();
+    }
+
+    @Override
+    public void onStart() {
+        Log.d ( TAG, "onStart() called" );
+        super.onStart ();
+    }
+
+    @Override
+    public void onResume() {
+        Log.d ( TAG, "onResume() called" );
+        super.onResume ();
+    }
+
+    @Override
+    public void onPause() {
+        Log.d ( TAG, "onPause() called" );
+        super.onPause ();
+    }
+
+    @Override
+    public void onStop() {
+        Log.d ( TAG, "onStop() called" );
+        super.onStop ();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        Log.d ( TAG, "onAttach() called with: context = [" + context + "]" );
+        super.onAttach ( context );
+    }
+
+    @Override
+    public void onDetach() {
+        Log.d ( TAG, "onDetach() called" );
+        super.onDetach ();
     }
 }
