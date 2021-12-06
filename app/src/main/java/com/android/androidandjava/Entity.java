58a1689d1423d63a248c3054f5d889ab92133cd7
Paveł <Paulik0182@gmail.com>
2021-12-06 16:41:45 +0300

    Constructor (описание сущьности)

---
+++ app/src/main/java/com/android/androidandjava/Entity.java
@@ -0,0 +1,27 @@
+package com.android.androidandjava;
+
+public class Entity {
+    private String title;
+    private String detail;
+
+    Entity(String title, String detail) {
+        this.title = title;
+        this.detail = detail;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+}
