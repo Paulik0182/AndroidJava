4abe88ed08b0dd6a007712ccd4cd98b8b7490aaf
Paveł <Paulik0182@gmail.com>
2021-12-08 15:29:07 +0300

    Add class EntityConstructor -> описание предметной облости. Конструктор (описание сущности), создаются поля описывающие предметную область, сущность.

---
+++ app/src/main/java/com/android/androidandjava/EntityConstructor.java
@@ -0,0 +1,28 @@
+package com.android.androidandjava;
+
+public class EntityConstructor {
+
+    private String title;
+    private String detail;
+
+    EntityConstructor(String title, String detail) {
+        this.title = title;
+        this.detail = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+}
