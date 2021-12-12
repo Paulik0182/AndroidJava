0e2a7bae54a92ab57750d3de12638ba298e95de6
Paveł <Paulik0182@gmail.com>
2021-12-12 22:22:16 +0300

    переход в SecondActivity - не работает

--- app/src/main/java/com/android/androidandjava/MainActivity.java
+++ app/src/main/java/com/android/androidandjava/MainActivity.java
@@ -1,10 +1,12 @@
 package com.android.androidandjava;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
@@ -38,6 +40,10 @@ public class MainActivity extends AppCompatActivity {
         public void onItemLongClickListener(EntityConstructor entityConstructor) {
             Toast.makeText ( MainActivity.this, "onItemLongClickListener ->" +
                     entityConstructor.getTitle (), Toast.LENGTH_LONG ).show ();
+
+            Intent intent = new Intent ( MainActivity.this, SecondActivity.class );
+            startActivity ( intent );
+
             Log.d ( TAG, "Listener Long" );
         }
     };
@@ -85,4 +91,40 @@ public class MainActivity extends AppCompatActivity {
         entities.add ( new EntityConstructor ( "Title_20", "Text" ) );
 
     }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState ( outState );
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState ( savedInstanceState );
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart ();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume ();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause ();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop ();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy ();
+    }
+
 }
--- app/src/main/java/com/android/androidandjava/SecondActivity.java
+++ app/src/main/java/com/android/androidandjava/SecondActivity.java
@@ -1,6 +1,58 @@
 package com.android.androidandjava;
 
-public class SecondActivity {
+import android.os.Bundle;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.androidandjava.databinding.ActivitySecondBinding;
+
+public class SecondActivity extends AppCompatActivity {
+    private static final String TAG = "@@@SecondActivity";
+
+    private ActivitySecondBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d ( TAG, "SecondActivity : onCreate() called with: savedInstanceState = [" + savedInstanceState + "]" );
+        super.onCreate ( savedInstanceState );
+        binding = ActivitySecondBinding.inflate ( getLayoutInflater () );
+        setContentView ( binding.getRoot () );
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState ( outState );
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState ( savedInstanceState );
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart ();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume ();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause ();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop ();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy ();
+    }
 }
