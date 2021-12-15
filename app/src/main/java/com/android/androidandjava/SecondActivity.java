12276bc6d8816135cb4cd7cac49efd2588683041
Paveł <Paulik0182@gmail.com>
2021-12-15 11:19:28 +0300

    save view LayoutSecond

--- app/src/main/java/com/android/androidandjava/SecondActivity.java
+++ app/src/main/java/com/android/androidandjava/SecondActivity.java
@@ -15,12 +15,13 @@ public class SecondActivity extends AppCompatActivity {
     private static final String TAG = "@@@SecondActivity";
 
     public static final String TITLE_OUT_EXTRA_KEY = "title_out_extra_key";
+    private static final String SAVE_RECEIVE_TITLE_KEY = "save_receive_title_key";
     public static final String DETAIL_OUT_EXTRA_KEY = "detail_out_extra_key";
     public static final String ENTITIES_OUT_EXTRA_KEY = "entities_out_extra_key";
 
     private ActivitySecondBinding binding;
 
-    private String receiveTitle = null;
+    private String receiveTitleSecondActivity = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,15 +36,15 @@ public class SecondActivity extends AppCompatActivity {
 //        }
 
         Intent intent = getIntent ();
-        receiveTitle = intent.getParcelableExtra ( MainActivity.SAVE_TITLE_KEY );
-        binding.echoTextView.setText ( getIntent ().getStringExtra ( receiveTitle ) );
+        receiveTitleSecondActivity = intent.getParcelableExtra ( MainActivity.SAVE_TITLE_KEY );
+        binding.echoTextView.setText ( getIntent ().getStringExtra ( String.valueOf ( receiveTitleSecondActivity ) ) );
 
         binding.okButton.setOnClickListener ( new View.OnClickListener () {
             @Override
             public void onClick(View v) {
                 Log.d ( TAG, "onClick() called with: v = [" + v + "]" );
                 Intent intent = new Intent ();
-                intent.putExtra ( TITLE_OUT_EXTRA_KEY, receiveTitle );
+                intent.putExtra ( TITLE_OUT_EXTRA_KEY, String.valueOf ( receiveTitleSecondActivity ) );
                 setResult ( Activity.RESULT_OK, intent );
                 finish ();
             }
@@ -52,11 +53,18 @@ public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
+        Log.d ( TAG, "onSaveInstanceState() called with: outState = [" + outState + "]" );
+        outState.putString ( SAVE_RECEIVE_TITLE_KEY, String.valueOf ( receiveTitleSecondActivity ) );
         super.onSaveInstanceState ( outState );
     }
 
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        Log.d ( TAG, "onRestoreInstanceState() called with: savedInstanceState = [" + savedInstanceState + "]" );
+        if (savedInstanceState.containsKey ( SAVE_RECEIVE_TITLE_KEY )) {
+            receiveTitleSecondActivity = savedInstanceState.getString ( SAVE_RECEIVE_TITLE_KEY, String.valueOf ( receiveTitleSecondActivity ) );
+        }
+        binding.echoTextView.setText ( String.valueOf ( receiveTitleSecondActivity ) );
         super.onRestoreInstanceState ( savedInstanceState );
     }
 
@@ -89,7 +97,7 @@ public class SecondActivity extends AppCompatActivity {
     public void onBackPressed() {
         Log.d ( TAG, "onBackPressed() called" );
         Intent intent = new Intent ();
-        intent.putExtra ( TITLE_OUT_EXTRA_KEY, receiveTitle );
+        intent.putExtra ( TITLE_OUT_EXTRA_KEY, String.valueOf ( receiveTitleSecondActivity ) );
         setResult ( Activity.RESULT_OK, intent );
         finish ();
     }
