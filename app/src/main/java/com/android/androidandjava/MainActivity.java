91fbcb612bb23b87102b9205952039f07509f0b1
Paveł <Paulik0182@gmail.com>
2022-01-11 13:21:52 +0300

    Binding

--- app/src/main/java/com/android/androidandjava/MainActivity.java
+++ app/src/main/java/com/android/androidandjava/MainActivity.java
@@ -1,14 +1,19 @@
 package com.android.androidandjava;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.androidandjava.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
-        setContentView ( R.layout.activity_main );
+        binding = ActivityMainBinding.inflate ( getLayoutInflater () );
+        setContentView ( binding.getRoot () );
     }
 }
