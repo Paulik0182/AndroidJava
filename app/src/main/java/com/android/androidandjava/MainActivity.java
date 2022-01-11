246473274f54e9d3efdbd97854346f529605f1d9
Paveł <Paulik0182@gmail.com>
2022-01-11 13:26:08 +0300

    MainActivity -> Log. Methods - onStart, onResume, onPause, onStop, onDestroy.

--- app/src/main/java/com/android/androidandjava/MainActivity.java
+++ app/src/main/java/com/android/androidandjava/MainActivity.java
@@ -1,6 +1,7 @@
 package com.android.androidandjava;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,12 +9,46 @@ import com.android.androidandjava.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "@@@ MainActivity";
+
     private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d ( TAG, "onCreate() called with: savedInstanceState = [" + savedInstanceState + "]" );
         super.onCreate ( savedInstanceState );
         binding = ActivityMainBinding.inflate ( getLayoutInflater () );
         setContentView ( binding.getRoot () );
     }
+
+    @Override
+    protected void onStart() {
+        Log.d ( TAG, "onStart() called" );
+        super.onStart ();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d ( TAG, "onResume() called" );
+        super.onResume ();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d ( TAG, "onPause() called" );
+        super.onPause ();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d ( TAG, "onStop() called" );
+        super.onStop ();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d ( TAG, "onDestroy() called" );
+        super.onDestroy ();
+    }
+
 }
